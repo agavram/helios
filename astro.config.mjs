dfeac17a860d089728dc82d36b1789a961a25220
Adrian Avram <Adrian_Avram@Outlook.com>
2023-11-07 14:08:22 -0800

    netlify build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import netlify from "@astrojs/netlify/functions";
 import { defineConfig } from 'astro/config';
 import serviceWorker from "astrojs-service-worker";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({
+  adapter: netlify({
     edgeMiddleware: true
   }),
   integrations: [solidJs(), tailwind(), serviceWorker()],
